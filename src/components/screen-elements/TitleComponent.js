a7a3260d380057481d7772503e8ec19072129e40
Zoltán Soós <zoli.soos98@gmail.com>
2024-05-03 15:29:22 +0200

    Create TitleComponent for landing page

---
+++ src/components/screen-elements/TitleComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function TitleComponent() {
+    return ( <span className='title-box'>
+        <span className='inner-title-box'>
+            C
+        </span>
+        <span className='inner-title-box'>
+            O
+        </span>
+        <span className='inner-title-box'>
+            U
+        </span>
+        <span className='inner-title-box'>
+            N
+        </span>
+        <span className='inner-title-box'>
+            T
+        </span>
+        <span className='inner-title-box'>
+            D
+        </span>
+        <span className='inner-title-box'>
+            O
+        </span>
+        <span className='inner-title-box'>
+            W
+        </span>
+        <span className='inner-title-box'>
+            N
+        </span>
+    </span> );
+}
+
+export default TitleComponent;
