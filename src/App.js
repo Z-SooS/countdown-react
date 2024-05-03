170552639b7e343bb66f63efa695586f666d74bd
Zoltán Soós <zoli.soos98@gmail.com>
2024-05-03 15:34:35 +0200

    Add commented code to load and clear word list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 
 function App() {
+  //TO-DO Uncomment when needed
+
+  /* const words = require("word-list-json");
+
+  words.length = words.lengths[9];
+  const firstFourLetter = words.lengths[3];
+  const gameWords = words.slice(firstFourLetter); */
   return (
     <div className="App">
     </div>
