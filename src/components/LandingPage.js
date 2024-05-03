ab6aa36db60306be4d40253a4bed531b2a974b38
Zoltán Soós <zoli.soos98@gmail.com>
2024-05-03 15:30:09 +0200

    Create LandingPage Component

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TitleComponent from './screen-elements/TitleComponent';
+
+function LandingPage() {
+    return ( <section id='landing-page'>
+        <div id='title'>
+            <TitleComponent/>
+        </div>
+        
+    </section> );
+}
+
+export default LandingPage;
