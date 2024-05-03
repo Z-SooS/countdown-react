b388b67ea91adff76aa9bbd57067fc3ba2638922
Zoltán Soós <zoli.soos98@gmail.com>
2024-05-03 16:07:05 +0200

    Add explanation and start button to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import TitleComponent from './screen-elements/TitleComponent';
 
-function LandingPage() {
+function LandingPage({startGameFunc}) {
     return ( <section id='landing-page'>
-        <div id='title'>
-            <TitleComponent/>
+        <h1 id='title'>
+            <TitleComponent/>sadasd
+        </h1>
+        <div id='game-explanation'>
+            <h4>"Countdown" is a popular British television game show that combines word and number puzzles.</h4>
+            <h5>Letter round: Select 9 consonants and vowels. Find the longest word you can form in 30 seconds</h5>
+            <h5>Number round: Select 6 small and big numbers. Try to get closest to the target number by basic arithmetic operations. (+, -, /, *)</h5>
         </div>
-        
+        <button id='start-button' onClick={startGameFunc}>
+            PLAY
+        </button>
     </section> );
 }
 
